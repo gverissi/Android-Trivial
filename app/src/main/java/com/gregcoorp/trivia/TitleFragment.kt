117648c7b76a8eb9a781552018c8e3b2ccdc074c
greg <greg.verissimo@gmail.com>
2020-05-03 18:39:41 +0200

    17. Exercise: Adding a Menu

--- app/src/main/java/com/gregcoorp/trivia/TitleFragment.kt
+++ app/src/main/java/com/gregcoorp/trivia/TitleFragment.kt
@@ -1,13 +1,12 @@
 package com.gregcoorp.trivia
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
-//import androidx.navigation.findNavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.gregcoorp.trivia.databinding.FragmentTitleBinding
 
 /**
@@ -35,7 +34,21 @@ class TitleFragment : Fragment() {
             Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
         )
 
+        // Tell Android that our TitleFragment has a menu
+        setHasOptionsMenu(true)
+
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        // Inflate our new menu resource using the provided menu inflater and menu structure
+        inflater.inflate(R.menu.overflow_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Connect the fragment_about to item about
+        return NavigationUI.onNavDestinationSelected(item, requireView().findNavController())
     }
 
 }
